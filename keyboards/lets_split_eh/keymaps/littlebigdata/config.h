8844bcf29c94a4e45f7a60481474feec0ffeacfa
Brian MacArthur <brian.macarthur@gmail.com>
2018-07-18 10:39:05 -0700

    Added layout for lets_split_eh:littlebigdata

--- keyboards/lets_split_eh/keymaps/littlebigdata/config.h
+++ keyboards/lets_split_eh/keymaps/littlebigdata/config.h
@@ -1,5 +1,8 @@
 /*
-Copyright 2017 Danny Nguyen <danny@hexwire.com>
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,16 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CONFIG_USER_H
 
 #include "../../config.h"
-
-/* Use I2C or Serial, not both */
-
-// #define USE_SERIAL
-#define USE_I2C
-
-/* Select hand configuration */
-
-// #define MASTER_LEFT
-#define MASTER_RIGHT
-// #define EE_HANDS
 
 #endif
