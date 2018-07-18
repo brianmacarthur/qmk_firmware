ac9831a360ecc6bf3d3f6376be17b8c5e97b9d6d
Brian MacArthur <brian.macarthur@gmail.com>
2018-07-18 10:40:32 -0700

    Update planck layout with dedicated Fn key instead of mixed.

--- keyboards/planck/keymaps/littlebigdata/keymap.c
+++ keyboards/planck/keymaps/littlebigdata/keymap.c
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_GRAVE,              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {LT(_FUNCTION, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT)},
+  {KC_TAB,                KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {MO(_FUNCTION),         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
   {KC_LCTL,               KC_LGUI, KC_LALT, KC_ESC,  LOWER,   KC_LSFT, KC_SPC,  RAISE,   KC_DEL,  KC_RALT, KC_RGUI, KC_RCTL} // GRID: 1U dedicated shift and space keys
   // {KC_LCTL,      KC_LGUI, KC_LALT, KC_ESC,  LOWER,MT(MOD_LSFT, KC_SPC),RAISE,   KC_ESC,  KC_RALT, KC_RGUI, KC_RCTL} // MIT: 2U space key with mod shift
 },
